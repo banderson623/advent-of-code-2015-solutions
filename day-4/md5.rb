9c6035c5b0e52603fa648adc9e5aace24a194aa9
Brian Anderson <banderson623@gmail.com>
2015-12-04 10:50:27 -0600

    oops, return to const

--- day-4/md5.rb
+++ day-4/md5.rb
@@ -3,14 +3,14 @@
 require 'digest/md5'
 
 MY_SECRET = 'ckczppom'
-i_want = (ARGV[0] || '00000') #change to 6 for the second part of the question
+I_WANT = (ARGV[0] || '00000') #change to 6 for the second part of the question
 
 number = 0;
 begin
   number += 1
   # printing slows it down a lot
   # print '.' if number % 1000 == 0
-end while Digest::MD5.hexdigest(MY_SECRET + number.to_s)[0,i_want.size] != i_want
+end while Digest::MD5.hexdigest(MY_SECRET + number.to_s)[0,I_WANT.size] != I_WANT
 
 puts "\nnumber: #{number} from my code #{MY_SECRET}"
 puts "md5: #{Digest::MD5.hexdigest(MY_SECRET + number.to_s)}"
